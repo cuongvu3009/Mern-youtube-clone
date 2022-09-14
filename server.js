309c4949bd3fc72add9e2eacfe257b7711094ce7
cuongvu3009 <cuongvu3009@gmail.com>
2022-09-14 10:14:47 +0300

    extra security

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./db/connectDB');
-const cookieParser = require('cookie-parser');
+
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
 
 const app = express();
 dotenv.config();
@@ -14,8 +19,22 @@ const userRouter = require('./routes/users');
 const videoRouter = require('./routes/videos');
 
 //middlewares
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
-app.use(cookieParser());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+app.get('/', (req, res) => {
+  res.send('<h1>Youtube-clone API</h1>');
+});
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/comments', commentRouter);
 app.use('/api/v1/users', userRouter);
